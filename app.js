32f40b65b60ab157e903fbd0cd035d3a4dfa25f4
GojoMoon <108343275+GojoMoon@users.noreply.github.com>
2022-06-30 19:52:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-//Ici du code JS ! 
-alert('Hi Mother F$ck')
+document.write("Hello World !"); //echo hello world
+alert("Who are you ?");
+ 
+//Displayed in the console
+console.clear();//Clear
+console.log("Problème ! ");//echo in console
+console.error("Cheloux !");//displayed error
+/*
+Comment in javascript
+*/
