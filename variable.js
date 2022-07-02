62b0b6a9cdb66b1e30e26652b4242b5dfb5d8e12
GojoMoon <108343275+GojoMoon@users.noreply.github.com>
2022-07-02 07:01:55 +0200

    Update variable.js

--- variable.js
+++ variable.js
@@ -5,19 +5,19 @@ var age = 23;
 var myVariable = "Variable";
 var real = true; //boolean
 
-document.write("My name is ", name, " and I am ", age, " years old."); //Echo 
+document.write("<br/>My name is ", name, " and I am ", age, " years old."); //Echo 
 
 age = 32;//Modifies a variable
 
-document.write("\nMy name is ", name, " and I am ", age, " years old. "); //Echo
-document.write(typeof age, ";");//Displays the type of variable
-document.write(typeof name, ";");//Displays the type of variable
+document.write("<br/>My name is ", name, " and I am ", age, " years old. "); //Echo
+document.write(typeof age, "<br/>");//Displays the type of variable
+document.write(typeof name, "<br/>");//Displays the type of variable
 document.write(typeof real);//Displays the type of variable
 
-var name2 = prompt("What is your name?");//Request to the user
-document.write(" ", name2);
+var name2 = prompt("<br/>What is your name?");//Request to the user
+document.write("<br/> ", name2);
 
-var age2 = prompt("How old are you?");
+var age2 = prompt("<br/>How old are you?");
 //Convert the type of the variable
 age2 = parseInt(age2); //Or parseFloat() for strings
 document.write(" ", typeof age2);
