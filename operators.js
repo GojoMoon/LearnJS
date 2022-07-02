2c676477749c4aea07258627427cd04102019fa4
GojoMoon <108343275+GojoMoon@users.noreply.github.com>
2022-07-02 07:10:33 +0200

    Create operators.js

---
+++ operators.js
@@ -0,0 +1,23 @@
+//operators
+/*
+	let data = prompt("How old are you?");
+	let data2 = data;
+*/
+
+let number = (4 + 3) * 6;
+document.write(number, "<br/>");
+
+let N = 420;
+N++;//N = N + 1 OR N++ OR ++N
+N -= 23;//N = N - 23 OR N-- OR --N
+--N;
+// =, +=, -=, *=, %=
+document.write(N, "<br/>");
+document.write(N++, "<br/>");
+document.write(N, "<br/>");
+//
+document.write(8 == 12, "<br/>");//Equality
+document.write(8 === "8", "<br/>");
+document.write(8 < 12, "<br/>");
+document.write(8 != 12, "<br/>");
+document.write(8 !== 12, "<br/>");
