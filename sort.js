6f5c7ea5c123653159c38be78285302cbf5ee4ac
GojoMoon <108343275+GojoMoon@users.noreply.github.com>
2022-07-20 13:14:43 +0200

    Update sort.js

--- sort.js
+++ sort.js
@@ -2,6 +2,7 @@
 
 let array = [1,10,5,15,2,7,28,900,45,18,27]
 let tab = ["Laura", "Bernard", "Salim", "Fatima", "Julien", "Franck"];
+let sort = [-2, 14, 1, 4, 92, 21, -123, -3, 0, 1];
 
 
 array.sort();
@@ -14,3 +15,7 @@ console.log(tab.sort(function(a, b)
 {
 	return a.localeCompare(b);
 }));
+console.log(sort.filter(x => x > 0));
+
+let rresult = tab.map(value => "Unknown");
+console.log(rresult);
